cf506a650f42d9c107da1b0ec552b3e3bed3a3b3
Quentinqv <quentin010801@gmail.com>
2023-04-15 10:17:32 +0200

    📦 NEW: Users basics API

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -6,31 +6,48 @@ import {
   Patch,
   Param,
   Delete,
+  UsePipes,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { HashPasswordPipe } from './hash-password/hash-password.pipe';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ValidationCreateUserPipe } from './validation-create-user/validation-create-user.pipe';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
-  }
-
   @Get()
+  @ApiResponse({ status: 200, description: 'Users found' })
+  @ApiResponse({ status: 404, description: 'Users not found' })
   findAll() {
     return this.usersService.findAll();
   }
 
   @Get(':id')
+  @ApiResponse({ status: 200, description: 'Users found' })
+  @ApiResponse({ status: 404, description: 'Users not found' })
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
 
+  @Post()
+  @ApiResponse({ status: 201, description: 'User created' })
+  @ApiResponse({ status: 400, description: 'Bad request' })
+  @UsePipes(HashPasswordPipe)
+  @UsePipes(ValidationCreateUserPipe)
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
   @Patch(':id')
+  @ApiResponse({ status: 200, description: 'User updated' })
+  @ApiResponse({ status: 400, description: 'Bad request' })
+  @UsePipes(HashPasswordPipe)
+  @UsePipes(ValidationCreateUserPipe)
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }

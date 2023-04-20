36b99e0b1852d722696d49806c5e6a2a462c0847
Vitoux Quentin <quentin010801@gmail.com>
2023-04-20 21:01:11 +0200

    📦 NEW: Init Categories

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { PostsModule } from './modules/posts/posts.module';
 import { VideosModule } from './modules/videos/videos.module';
+import { CategoriesModule } from './modules/categories/categories.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, PostsModule, VideosModule],
+  imports: [UsersModule, AuthModule, PostsModule, VideosModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })

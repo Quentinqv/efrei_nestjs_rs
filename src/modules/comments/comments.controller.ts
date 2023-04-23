2ad7a3378c36bbe58facc012fba23f862149fb2c
willekenselise <willekens.elise@orange.fr>
2023-04-23 03:10:19 +0200

    NEW: CRUD Comment

--- src/modules/comments/comments.controller.ts
+++ src/modules/comments/comments.controller.ts
@@ -1,28 +1,46 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('comments')
 @Controller('comments')
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
 
-  @Post()
-  create(@Body() createCommentDto: CreateCommentDto) {
-    return this.commentsService.create(createCommentDto);
-  }
-
   @Get()
+  @ApiResponse({ status: 200, description: 'Comments found' })
+  @ApiResponse({ status: 404, description: 'Comments not found' })
   findAll() {
     return this.commentsService.findAll();
   }
 
   @Get(':id')
+  @ApiResponse({ status: 200, description: 'Comments found' })
+  @ApiResponse({ status: 404, description: 'Comments not found' })
   findOne(@Param('id') id: string) {
     return this.commentsService.findOne(+id);
   }
 
+  @Post()
+  @ApiResponse({ status: 201, description: 'Posts created' })
+  @ApiResponse({ status: 400, description: 'Bad request' })
+  create(@Body() createCommentDto: CreateCommentDto) {
+    return this.commentsService.create(createCommentDto);
+  }
+
   @Patch(':id')
+  @ApiResponse({ status: 201, description: 'Posts updated' })
+  @ApiResponse({ status: 400, description: 'Bad request' })
   update(@Param('id') id: string, @Body() updateCommentDto: UpdateCommentDto) {
     return this.commentsService.update(+id, updateCommentDto);
   }
--- src/modules/comments/comments.service.ts
+++ src/modules/comments/comments.service.ts
@@ -1,26 +1,44 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class CommentsService {
-  create(createCommentDto: CreateCommentDto) {
-    return 'This action adds a new comment';
+  constructor(private prisma: PrismaService) {}
+
+  async findAll() {
+    return await this.prisma.comment.findMany();
   }
 
-  findAll() {
-    return `This action returns all comments`;
+  async findOne(id: number) {
+    return await this.prisma.comment.findUnique({
+      where: {
+        id,
+      },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} comment`;
+  async create(createCommentDto: CreateCommentDto) {
+    return await this.prisma.comment.create({
+      data: createCommentDto,
+    });
   }
 
-  update(id: number, updateCommentDto: UpdateCommentDto) {
-    return `This action updates a #${id} comment`;
+  async update(id: number, updateCommentDto: UpdateCommentDto) {
+    return await this.prisma.comment.update({
+      where: {
+        id,
+      },
+      data: updateCommentDto,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} comment`;
+  async remove(id: number) {
+    return await this.prisma.comment.delete({
+      where: {
+        id,
+      },
+    });
   }
 }
